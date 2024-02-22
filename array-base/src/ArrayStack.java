f8d9f09507d84c6e771a739fd361eff8489bc167
IDRIS ABDELLAH <idrisabdellah@outlook.fr>
2024-02-22 23:10:42 +0100

    Stack: operations

--- array-base/src/ArrayStack.java
+++ array-base/src/ArrayStack.java
@@ -38,42 +38,35 @@ public class ArrayStack < T > extends AbstractList < T > {
         return n;
     }
 
-    // Replaces the element at index i in the array a with the given element x and returns the original element
-    public T set(int i, T x) {
-        if (i < 0 || i > n - 1) throw new IndexOutOfBoundsException();
-        T y = a[i];
-        a[i] = x;
-        return y;
-    }
-
     // Inserts the element x at the specified index i in the array a
-    public void add(int i, T x) {
-        if (i < 0 || i > n) throw new IndexOutOfBoundsException();
+    public void push(T x) {
         if (n + 1 > a.length) resize();
         //  Optimisation : System.arraycopy(a, i, a, i + 1, n - i);
-        for (int j = n; j > i; j--)
-            a[j] = a[j - 1];
-        a[i] = x;
+        a[n] = x;
         n++;
     }
 
-    // Removes and returns the element at the specified index i in the array a
-    public T remove(int i) {
-        if (i < 0 || i > n - 1) throw new IndexOutOfBoundsException();
-        T x = a[i];
-        // Optimisation : System.arraycopy(a, i + 1, a, i, n - i - 1);
-        for (int j = i; j < n - 1; j++)
-            a[j] = a[j + 1];
-        n--;
+    // Pops the top element from the stack
+    public T pop() {
+        T x = a[n-1]; // Decreasing the count and then removing from the top
+        a[--n]=null;
         if (a.length >= 3 * n) resize();
         return x;
     }
 
-    // Pops the top element from the stack
-    public T remove() {
-        T x = a[--n]; // Decreasing the count and then removing from the top
-        if (a.length >= 3 * n) resize();
-        return x;
+    private boolean empty(){
+        return n == 0;
+    }
+
+    private T peek(){
+        return a[n-1];
+    }
+
+    private boolean search(T x){
+        for (T t : a) {
+            if (t == x) return true;
+        }
+        return false;
     }
 
     /*
@@ -85,16 +78,28 @@ public class ArrayStack < T > extends AbstractList < T > {
 
         System.out.println("Stack Operations");
 
+
+
+
         ArrayStack < Integer > stack = new ArrayStack < > (Integer.class);
+        System.out.println("empty State: " + stack.empty());
         // Adds the elements to the stack
-        stack.add(0, 1);
-        stack.add(1, 2);
-        stack.add(2, 3);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
 
-        System.out.println("Stack size: " + stack.size());
+        System.out.println("Stack number of elements: " + stack.size());
+        System.out.println("empty State: " + stack.empty());
+        System.out.println("Peek: " + stack.peek());
 
-        int removedElement = stack.remove(1);
+        System.out.println("element 3: " + stack.search(3));
+
+        int removedElement = stack.pop();
         System.out.println("Removed element: " + removedElement);
+
+
+        System.out.println("element 3: " + stack.search(3));
+
 
         System.out.println("Stack elements:");
         for (int i = 0; i < stack.size(); i++) {
@@ -108,9 +113,9 @@ public class ArrayStack < T > extends AbstractList < T > {
         ArrayStack<Integer> stackOptimisation = new ArrayStack<>(Integer.class);
 
         // Adding a large number of elements to the stack
-        int numElements = 1000000; // Add 1 million elements
+        int numElements = 1_000_000; // Add 1 million elements
         for (int i = 0; i < numElements; i++) {
-            stackOptimisation.add(i);
+            stackOptimisation.push(i);
         }
 
         // Measuring time taken by resize()
